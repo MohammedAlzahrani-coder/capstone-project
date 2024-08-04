6f9c36a9ad56d096ecdafad8bc051b7fdfe3f4d1
Beq <moalsaadi18@gmail.com>
2024-08-04 15:13:06 +0300

    The merchantstock CRUD is done and is tested with postman

--- src/main/java/com/example/capstone1/Service/MerchantStockService.java
+++ src/main/java/com/example/capstone1/Service/MerchantStockService.java
@@ -1,4 +1,59 @@
 package com.example.capstone1.Service;
 
+import com.example.capstone1.Model.MerchantStock;
+import com.example.capstone1.Model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+
+@Service
 public class MerchantStockService {
+
+
+    ArrayList<MerchantStock> merchantStocks = new ArrayList<>();
+
+
+
+    public ArrayList<MerchantStock> getMerchantStocks(){
+
+        return merchantStocks;
+
+    }
+
+    public void addMerchantStock(MerchantStock merchantStock){
+
+        merchantStocks.add(merchantStock);
+
+    }
+
+    public boolean updateMerchantStock(int id, MerchantStock merchantStock){
+
+        for (int i = 0; i < merchantStocks.size() ; i++) {
+            if(merchantStocks.get(i).getId() == id){
+                merchantStocks.set(i, merchantStock);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public boolean deleteMerchantStock(int id){
+        for (MerchantStock merchantStock: merchantStocks){
+
+            if(merchantStock.getId() == id){
+
+                merchantStocks.remove(merchantStock);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
 }
